fb8a49222e3fc3bde9ae5a3bbb4d75ce39257a82
Dusan Chromy <dusan.chromy@deutschebahn.com>
2024-08-20 14:54:27 +0200

    Rename bicep and parameterize deploy script

--- quickstarts/microsoft.machinelearningservices/machine-learning-workspace/deploy.sh
+++ quickstarts/microsoft.machinelearningservices/machine-learning-workspace/deploy.sh
@@ -1,1 +1,3 @@
-az deployment group create --resource-group azureml-rg-aipd218 --template-file main.bicep
+TEMPLATE=$1
+
+az deployment group create --resource-group azureml-rg-aipd218 --template-file {$TEMPLATE:-main.bicep}
